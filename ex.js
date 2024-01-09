5ed48263f9409be4277dd10a734cb9be2bcb5346
marciojrdev1998 <marciojrgalvao@gmail.com>
2024-01-09 20:07:08 -0300

    subindo exemplos de aulas

---
+++ ex.js
@@ -0,0 +1,5 @@
+const divisao = 10 / 10;
+const subtracao = 10 - 10;
+const operacao = (divisao + subtracao) + 10;
+
+console.log(divisao + subtracao) + 10;
---
+++ script.js
@@ -0,0 +1,36 @@
+// var 
+
+//var comprimento = 7;
+//var altura = 5;
+
+//area = altura * comprimento;
+//console.log(area)
+
+//let forma = 'retângulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area = 7;
+
+//if (forma === 'retangulo') {
+//    area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento) / 2;
+//}
+
+//area = altura * comprimento
+//console.log(area)
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
+
+
