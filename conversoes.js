3a6eeb841301aab853b79e936f4a627ed2f65488
marciojrdev1998 <marciojrgalvao@gmail.com>
2024-01-11 20:07:39 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dados
+// booleans
+
+// conversão implicíta
+const numero = 456;
+const numeroString = Number("456a");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explicíta
